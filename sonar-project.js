c76e954a4cef41040bb34422196804521509bb1c
erickarroyo1 <50220864+erickarroyo1@users.noreply.github.com>
2019-11-11 13:59:43 -0500

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,6 +1,6 @@
 const sonarqubeScanner = require('sonarqube-scanner');
      sonarqubeScanner({
-       serverUrl: 'http://3.16.49.91/:9000',
+       serverUrl: 'http://3.16.49.91:9000',
        options : {
        'sonar.sources': '.',
        //'sonar.inclusions' : '.' // Entry point of your code
